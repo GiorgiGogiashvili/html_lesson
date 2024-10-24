41ec53f8ca9c2fff83cdc5c49848593bc51266f2
Giorgi Gogiashvili <133604374+GiorgiGogiashvili@users.noreply.github.com>
2024-10-25 01:09:42 +0400

    Add files via upload
    Js script

---
+++ html/script.js
@@ -0,0 +1,4 @@
+alert("Приветствую тебя пользователь!")
+myParagraph.addEventListener("click", function() {
+    myParagraph.textContent = "Сейчас я учу JavaScript, HTML и CSS. Надеюсь все получится :)";
+    });
